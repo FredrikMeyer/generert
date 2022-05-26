25c9fe09cfc08d2f9d612a8e2c897ace7a85b82c
Fredrik Meyer <hrmeyer@gmail.com>
2022-05-26 11:36:42 +0200

    Delete unused files

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject generert "0.1.1-SNAPSHOT"
   :description "Generert..."
-  :url "https://fredrikmeyer.net/FIXME"
+  :url "https://fredrikmeyer.net/"
   :main generert.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}

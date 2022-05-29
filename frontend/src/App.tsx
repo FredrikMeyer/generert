c9ed42d0f8c26a8d72ced857d33e4e1a97d3fb7a
Fredrik Meyer <hrmeyer@gmail.com>
2022-05-29 21:33:45 +0200

    Remove chakra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,41 +2,49 @@ import "./App.css";
 import concentric from "./images/concentric.png";
 import moon from "./images/moon.png";
 import colorCircle from "./images/color_circle.png";
-import { Container, SimpleGrid, Text, Center, Box } from "@chakra-ui/react";
 import { useState } from "react";
 
 const images = [concentric, moon, colorCircle];
 
 function Image({ src }: { src: string }) {
-  return (
-    <Center>
-      <img src={src} />
-    </Center>
-  );
+  return <img src={src} style={{ objectFit: "contain" }} />;
 }
 
 function App() {
   const [activeImage, setActiveImage] = useState(0);
 
   return (
-    <SimpleGrid minH="100vh" gap="0" paddingTop="1vh" paddingBottom="1vh">
-      <Container alignItems="center">
-        <Center>
-          <Text fontSize="6xl">Generert</Text>
-        </Center>
-      </Container>
-      <Box padding="20px">
-        <Container
-          border="1px solid black"
-          borderRadius="5px"
-          padding="1vh"
-          maxW="container.sm"
-          onClick={() => setActiveImage((activeImage + 1) % images.length)}
-        >
-          <Image src={images[activeImage]} />
-        </Container>
-      </Box>
-    </SimpleGrid>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <header>
+        <div style={{ margin: "auto", textAlign: "center" }}>
+          <h1>Generert</h1>
+        </div>
+      </header>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "1vh",
+          height: "80%",
+          width: "80%",
+          textAlign: "center",
+        }}
+        onClick={() => setActiveImage((activeImage + 1) % images.length)}
+      >
+        <Image src={images[activeImage]} />
+      </div>
+    </div>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-
 import App from "./App";
 import "./index.css";
 
@@ -9,9 +7,7 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <App />
     </React.StrictMode>
   );
 }

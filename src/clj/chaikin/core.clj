42265f24f6a798b59cd9de888ee38aa71245a073
Fredrik Meyer <hrmeyer@gmail.com>
2022-08-24 21:50:36 +0200

    More Chaikin experients

--- src/clj/chaikin/core.clj
+++ src/clj/chaikin/core.clj
@@ -13,19 +13,15 @@
   (q/stroke 100 10)
   {})
 
-(defn random-pt
-  "Generate a random point inside the square bunded by [x0, x1] and [y0, y1]."
-  [[x0 x1] [y0 y1]]
-  [(r/random x0 x1)
-   (r/random y0 y1)])
+(defn copy-first-to-end
+  "Copy the first element of a seq to the end."
+  [xs]
+  (let [frst (first xs)]
+    (concat xs [frst])))
 
-(defn random-pts
-  "Generates n random points bounded by [x0, x1] and [y0, y1]. See [[random-pt]]."
-  [n [x0 x1] [y0 y1]]
-
-  (for [_ (range n)] (random-pt [x0 x1] [y0 y1])))
-
-(defn draw-points [pts]
+(defn draw-points
+  "Given a list of points, draw lines connecting them."
+  [pts]
   (loop [ps pts]
     (when (>= (count ps) 2)
       (let [[p1 p2] (take 2 ps)]
@@ -35,51 +31,58 @@
 (defn iterate-chaikin
   "Given a list of points, use the Chaikin algorithm to smooth the line between them. If ratio
   is not given, uses default value of 0.75."
-  ([pts]
-   (iterate-chaikin pts 0.25))
-  ([pts ratio]
-   (loop [ps pts
-          new []]
-     (if (>= (count ps) 2)
-       (let [[a b] (take 2 ps)
-             q (gtl/point-on-line a b ratio)
-             r (gtl/point-on-line a b (- 1 ratio))]
-         ;; (println a b q r)
-         (recur (rest ps) (conj new q r)))
-       new))))
+  [pts & {:keys [closed ratio] :or {closed false ratio 0.25}}]
+  (let [pts-new pts
+        segments (partition 2 1 pts-new)
+        new-pts (mapcat (fn [[a b]]
+                          (let [q (gtl/point-on-line a b ratio)
+                                r (gtl/point-on-line a b (- 1 ratio))]
+                            [q r])) segments)]
+    (if closed
+      (copy-first-to-end new-pts)
+      new-pts)))
 
 (defn chaikin-curve
-  ([pts n]
-   (nth (iterate iterate-chaikin pts) n)
-   )
-  ([pts n ratio]
-   (nth (iterate (fn [pts] (iterate-chaikin pts ratio)) pts) n) ))
-
-(defn copy-first-to-end [xs]
-  (let [frst (first xs)]
-    (concat xs [frst])))
+  "Generate a Chaikin curve from a list of points. If ratio is not given
+   uses default value of 0.25."
+  [pts n & {:keys [ratio closed] :or {closed false ratio 0.25}}]
+  (nth (iterate (fn [pts] (iterate-chaikin pts :ratio ratio :closed closed)) pts) n))
 
 (defn draw []
   (q/stroke 100)
 
-  (draw-points (chaikin-curve (copy-first-to-end [[100 800] [800 800] [450 100]]) 3
-                              )
-               )
-  (comment 
+  (comment
     (draw-points
-     (chaikin-curve 
-      (copy-first-to-end (random-pts 5 [0 900] [0 900])) 0
-      )))
-  (comment 
+     (chaikin-curve
+      (copy-first-to-end [[100 800] [800 800] [450 100]]) 1)))
+
+  (comment
+    (draw-points
+     (chaikin-curve
+      (copy-first-to-end (r/random-pts 5 [0 900] [0 900])) 5)))
+
+  (comment (doseq [x (range 50 850 100)
+                   y (range 50 850 50)]
+             (let [pts (r/random-pts 5 [x (+ x 100)] [y (+ y 100)])
+                   chaikined (chaikin-curve pts 5 :closed true)]
+               (draw-points chaikined))))
+
+  (doseq [y (range 50 850 20)]
+    (let [xs (range 50 850 20)
+          x-pts (mapcat identity
+                        (for [x xs]
+                          (r/random-pts 10 [x (+ x 15)] [y (+ y 20)])))]
+      (->> x-pts
+           ((fn [ps] (chaikin-curve ps 2 :closed false :ratio 0.25)))
+           draw-points)))
+
+  (comment
     (doseq [x (range 50 850 100)
             y (range 50 850 100)]
-      (let [pts (random-pts 10 [x (+ x 100)] [y (+ y 100)])
+      (let [pts (r/random-pts 10 [x (+ x 100)] [y (+ y 100)])
             pts (copy-first-to-end pts)
             chaikined (chaikin-curve pts 5)]
-        (draw-points chaikined)
-        ))))
-
-partition
+        (draw-points chaikined)))))
 
 (defn update-state [state]
   state)
@@ -95,7 +98,7 @@ partition
   :size [w h]
   :setup setup
   :update update-state
-  :mouse-clicked d/save-on-click-handler
+  :mouse-clicked (d/save-on-click-handler "chaikin")
   :key-pressed d/redraw
   :draw draw-state
   :features [:keep-on-top :no-bind-output :pause-on-error]
--- src/clj/tools/random.clj
+++ src/clj/tools/random.clj
@@ -7,3 +7,15 @@
    (* (rand) max))
   ([min max]
    (+ min (random (- max min)))))
+
+(defn random-pt
+  "Generate a random point inside the square bunded by [x0, x1] and [y0, y1]."
+  [[x0 x1] [y0 y1]]
+  [(random x0 x1)
+   (random y0 y1)])
+
+(defn random-pts
+  "Generates n random points bounded by [x0, x1] and [y0, y1]. See [[random-pt]]."
+  [n [x0 x1] [y0 y1]]
+
+  (for [_ (range n)] (random-pt [x0 x1] [y0 y1])))

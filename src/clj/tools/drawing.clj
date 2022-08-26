7d6e0314e2beb78c5224d0102999f977ebd55c2d
Fredrik Meyer <hrmeyer@gmail.com>
2022-08-26 23:31:45 +0200

    Save in images folder

--- src/clj/tools/drawing.clj
+++ src/clj/tools/drawing.clj
@@ -1,12 +1,14 @@
 (ns tools.drawing
   (:require [quil.core :as q]))
 
-(defn save-on-click-handler [name]
-  (fn [state event]
+(defn save-on-click-handler
+  "Returns a function that returns a save-handler for your sketch."
+  [name]
+  (fn [state _]
     (println "Saved")
     (println state)
     (println name)
-    (q/save-frame (str name (hash state) "_" (q/random 0 1) ".tif"))
+    (q/save-frame (str "images/" name (hash state) "_" (q/random 0 1) ".tif"))
     state))
 
 (defn redraw [old-state event]

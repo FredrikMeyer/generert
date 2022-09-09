cc7cfadd2fce512b822a8a679a81ec2c55fca3ef
Fredrik Meyer <hrmeyer@gmail.com>
2022-09-09 22:22:03 +0200

    Add new design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,84 @@
 import "./App.css";
-import concentric from "./images/concentric.png";
-import moon from "./images/moon.png";
-import colorCircle from "./images/color_circle.png";
 import { useState } from "react";
+import texture from "./images/texture-15128758_0.43535608.png";
+import texture2 from "./images/texture-15128758_0.058291435.png";
+import concentric from "./images/concentric-15128758_0.9700706.png";
 
-const images = [
-  {
-    url: concentric,
-    name: "Concentric",
-  },
-  {
-    url: moon,
-    name: "Moon",
-  },
-  {
-    url: colorCircle,
-    name: "Colored system",
-  },
-];
+function Header() {
+  return (
+    <div
+      style={{
+        borderBottom: "3px double black",
+        marginBottom: "5px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Generert</h1>
+    </div>
+  );
+}
+
+function Gallery({
+  setActiveImage,
+}: {
+  setActiveImage: (image: string) => void;
+}) {
+  const images = [texture, texture2, concentric];
+  return (
+    <div className="images">
+      {images.map((i) => (
+        <div key={i} onClick={() => setActiveImage(i)} className="image">
+          <img
+            src={i}
+            style={{
+              borderRadius: "50%",
+              objectFit: "cover",
+              width: "100%",
+              maxHeight: "100%",
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function SingleImage({ close, image }: { image: string; close: () => void }) {
+  return (
+    <div
+      onClick={() => close()}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img src={image} />
+    </div>
+  );
+}
 
 function App() {
-  const [activeImage, setActiveImage] = useState(0);
+  const [activeImage, setActiveImage] = useState<null | string>(null);
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        height: "100%",
-        justifyContent: "space-around",
-        alignItems: "center",
+        height: "100vh",
+        maxHeight: "100vh",
       }}
     >
-      <header>
-        <div style={{ margin: "auto", textAlign: "center" }}>
-          <h1>Generert</h1>
-        </div>
-      </header>
-
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          border: "1px solid black",
-          borderRadius: "5px",
-          padding: "1vh",
-          height: "80%",
-          width: "80%",
-          textAlign: "center",
-        }}
-        onClick={() => setActiveImage((activeImage + 1) % images.length)}
-      >
-        <img src={images[activeImage].url} alt={images[activeImage].name} />
-      </div>
+      <Header />
+      <main>
+        {activeImage ? (
+          <SingleImage close={() => setActiveImage(null)} image={activeImage} />
+        ) : (
+          <Gallery setActiveImage={setActiveImage} />
+        )}
+      </main>
+      <div className="footer">Fredrik Meyer</div>
     </div>
   );
 }

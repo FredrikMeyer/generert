cea4b64947e11a229c844f40afc5dc1ae58ff8a0
Fredrik Meyer <hrmeyer@gmail.com>
2024-04-09 16:10:47 +0200

    fix failing test

--- test/tools/lines_test.clj
+++ test/tools/lines_test.clj
@@ -7,5 +7,5 @@
   (testing "Test point on line function"
     (let [a (t/point-on-line [0 0] [1 0] 0)
           b (t/point-on-line [0 0] [1 0] 1)]
-      (is (= a [0 0]))
-      (is (= b [1 0])))))
+      (is (= a [0. 0.]))
+      (is (= b [1. 0.])))))

0c4cff5c5b4bc2d6fab9fc81fbedf8368e235ce2
Fredrik Meyer <hrmeyer@gmail.com>
2024-08-04 18:27:29 +0200

    Ny eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,33 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { fixupPluginRules } from "@eslint/compat";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    languageOptions: {
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser },
+    },
+  },
+  {
+    plugins: {
+      react: eslintPluginReact,
+      "react-hooks": fixupPluginRules(eslintPluginReactHooks),
+    },
+  },
+  {
+    rules: {
+      // ...
+      ...eslintPluginReactHooks.configs.recommended.rules,
+    },
+  },
+);

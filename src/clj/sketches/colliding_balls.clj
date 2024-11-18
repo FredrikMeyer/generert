f406cfcd74e9b01459be958e57f84e662f4f6ee4
Fredrik Meyer <hrmeyer@gmail.com>
2024-11-18 15:51:39 +0100

    Protocols for update-pos

--- src/clj/sketches/colliding_balls.clj
+++ src/clj/sketches/colliding_balls.clj
@@ -16,34 +16,42 @@
   (q/no-stroke)
   (q/frame-rate 60)
   (q/smooth)
-  {:objects [{:shape (shapes/->Circle [450 450] 50)
-              :direction [2 1]
+  {:objects [#_{:shape (shapes/->Circle [450 450] 20)
+                :direction [2 1]
+                :color :red
+                :path []}
+             #_{:shape (shapes/->Circle [200 450] 15)
+                :direction [1 2]
+                :color :blue
+                :path []}
+             #_{:shape (shapes/->Circle [600 850] 20)
+                :direction [1 2]
+                :color :green
+                :path []}
+             {:shape (shapes/->Circle [700 700] 10)
+                :direction [1 2]
+                :color :teal
+                :path []}
+             {:shape ( shapes/update-pos (shapes/->Rectangle 30 50 150 150) [300 300])
+              :direction [1 -2]
+              :color :yellow
+              :path []}
+             {:shape ( shapes/update-pos (shapes/->Rectangle 30 50 150 150) [150 150])
+              :direction [2 -2]
               :color :red
               :path []}
-             {:shape (shapes/->Circle [200 450] 50)
-              :direction [1 2]
-              :color :blue
-              :path []}
-             {:shape (shapes/->Circle [600 850] 50)
-              :direction [1 2]
-              :color :green
-              :path []}
-             {:shape (shapes/->Circle [700 700] 50)
-              :direction [1 2]
-              :color :teal
-              :path []}
-             {:shape (shapes/->Circle [700 350] 50)
-              :direction [1 -2]
-              :color :ochre
-              :path []}
-             {:shape (shapes/->Circle [590 350] 50)
-              :direction [1 -2]
-              :color :burgundy
-              :path []}
-             {:shape (shapes/->Circle [150 700] 50)
-              :direction [3 -2]
-              :color :pink
-              :path []}]})
+             #_{:shape (shapes/->Circle [700 350] 10)
+                :direction [3 -2]
+                :color :ochre
+                :path []}
+             #_{:shape (shapes/->Circle [590 350] 20)
+                :direction [1 -2]
+                :color :burgundy
+                :path []}
+             #_{:shape (shapes/->Circle [150 700] 30)
+                :direction [3 -2]
+                :color :pink
+                :path []}]})
 
 (defn color->value [color]
   (cond (= :red color)
@@ -58,30 +66,32 @@
         [92 92 67]
         (= :ochre color)
         [8 71 86]
+        (= :yellow color)
+        [10 100 100]
         [= :teal color]
         [50 100 25]))
 
 (defn collides? [object objects]
   {:pre [(some? object)]}
-  (some #(shapes/circle-intersect-circle
+  (some #(shapes/intersects
           (:shape object) (:shape %))
         (filter #(not (= (:shape %) (:shape object))) objects)))
 
 (defn update-pos [obj objects]
-  (let [prev-pos (-> obj :shape :center)
+  (let [prev-pos (-> obj :shape shapes/position)
         direction (-> obj :direction)
-        new-dir (p/mult 20 (p/normalize (p/add-pts
-                                        (p/mult 0.5
-                                                (p/diff-pts (p/mult 2 (r/random-pt)) [1 1]))
-                                        (if (or
-                                             (collides? obj objects)
-                                             (> (p/distance-sq [(* 0.5 w) (* 0.5 h)]
-                                                               prev-pos) (* 450 450)))
-                                          (p/mult -1 direction)
-                                          direction))))
+        new-dir (p/mult 5 (p/normalize (p/add-pts
+                                         (p/mult 0.5
+                                                 (p/diff-pts (p/mult 2 (r/random-pt)) [1 1]))
+                                         (if (or
+                                              (collides? obj objects)
+                                              (> (p/distance-sq [(* 0.5 w) (* 0.5 h)]
+                                                                prev-pos) (* 450 450)))
+                                           (p/mult -1 direction)
+                                           direction))))
         new-pos (p/add-pts prev-pos new-dir)]
     (-> obj
-        (assoc-in [:shape :center] new-pos)
+        (update :shape (fn [old] (shapes/update-pos old new-pos)))
         (assoc :direction new-dir))))
 
 (defn update-state [state]
@@ -89,7 +99,8 @@
       (update :objects (fn [objs] (map #(update-pos % objs) objs)))
       (update :objects (fn [objs]
                          (map #(update % :path
-                                       (fn [old] (conj old (-> % :shape :center)))) objs)))))
+                                       (fn [old]
+                                         (conj old (-> % :shape shapes/position)))) objs)))))
 
 (defn draw-state [state]
   (q/with-fill [0 1]
@@ -98,11 +109,11 @@
   (let [objects (:objects state)]
     (doseq [object objects]
       (let [shape (:shape object)
-            [x y] (:center shape)
-            radius (:radius shape)
             path (:path object)]
         (q/with-fill (color->value (:color object))
-          (q/ellipse x y (* 2 radius) (* 2 radius)))
+          (shapes/draw shape)
+          ;; (q/ellipse x y (* 2 radius) (* 2 radius))
+          )
 
         (comment
           (q/with-stroke (color->value (:color object))
--- src/clj/tools/shapes.clj
+++ src/clj/tools/shapes.clj
@@ -9,35 +9,32 @@
 (defprotocol Drawable
   (draw [this]))
 
-(defrecord Circle [center radius]
-  Intersects
+(defprotocol Positioned
+  (position [this]))
 
+(defprotocol Movable
+  (update-pos [this new-pos]))
+
+(defrecord Circle [center radius]
   Drawable
   (draw [{:keys [center radius]}]
     (let [[x y] center]
-      (q/ellipse x y radius radius))))
+      (q/ellipse x y (* 2 radius) (* 2 radius)))))
 
-(defrecord Point [x y])
+(defrecord Point [x y]
+  Drawable
+  (draw [_]
+    (q/ellipse x y 2 2)))
 
 (defrecord Rectangle [^double xmin ^double ymin ^double xmax ^double ymax]
   Drawable
-  (draw [this]
+  (draw [_]
     (q/rect xmin ymin (- xmax xmin) (- ymax ymin))))
-
-(defmulti intersect-shapes
-  (fn [s1 s2]
-    (into #{} '(s1 s2))))
 
 (defn pt-intersect-circle [pt circle]
   (let [dist-center (Math/sqrt (pts/distance-sq pt (:center circle)))
         dist (- dist-center (:radius circle))]
     (<= dist 0)))
-
-(defn circle-intersect-circle [c1 c2]
-  (let [dist-centers (Math/sqrt (pts/distance-sq (:center c1)
-                                                 (:center c2)))
-        [r1 r2] (mapv :radius [c1 c2])]
-    (<= dist-centers (+ r1 r2))))
 
 (defn point-intersect-rectangle [pt rect]
   (let [[x y] pt
@@ -47,13 +44,6 @@
          (<= x xmax)
          (<= y ymax))))
 
-(defn rectangle-intersect-rectangle [r1 r2]
-  (let [{:keys [xmin ymin xmax ymax]} r1]
-    (not (or (> ymin (:ymax r2))
-             (< ymax (:ymin r2))
-             (> xmin (:xmax r2))
-             (< xmax (:xmin r2))))))
-
 (defn closest-edge [{:keys [center]} {:keys [xmin xmax ymin ymax]}]
   (let [[cx cy] center]
     [(cond (< cx xmin) xmin
@@ -62,6 +52,13 @@
      (cond (< cy ymin) ymin
            (> cy ymax) ymax
            :else cy)]))
+
+(defn rectangle-intersect-rectangle [r1 r2]
+  (let [{:keys [xmin ymin xmax ymax]} r1]
+    (not (or (> ymin (:ymax r2))
+             (< ymax (:ymin r2))
+             (> xmin (:xmax r2))
+             (< xmax (:xmin r2))))))
 
 (defn rectangle-intersect-circle
   "From here: https://www.jeffreythompson.org/collision-detection/circle-rect.php"
@@ -74,4 +71,69 @@
         (let [edge (closest-edge circle rect)]
           (< (pts/distance-sq edge (:center circle)) (Math/pow (:radius circle) 2))))))
 
+(defn circle-intersect-circle [c1 c2]
+  (let [dist-centers (Math/sqrt (pts/distance-sq (:center c1)
+                                                 (:center c2)))
+        [r1 r2] (mapv :radius [c1 c2])]
+    (<= dist-centers (+ r1 r2))))
 
+(extend-protocol Intersects
+  Circle
+  (intersects [this other]
+    (let [other-class (class other)]
+      (cond (= other-class (class this))
+            (circle-intersect-circle this other)
+            (= other-class Rectangle)
+            (rectangle-intersect-circle other this)
+            :else (throw (Exception. (str "Ops. Other class: " (class other)))))))
+
+  Rectangle
+  (intersects [this other]
+    (let [other-class (class other)]
+      (cond (= other-class (class this))
+            (rectangle-intersect-rectangle this other)
+            (= other-class tools.shapes.Circle)
+            (rectangle-intersect-circle this other)
+            :else (throw (Exception. (str "Ops. Other class: " (class other)))))))
+
+  Point
+  (intersects [this other]
+    (let [other-class (class other)]
+      (cond (= other-class (class this))
+            (= this other)
+            (= other-class Circle)
+            (pt-intersect-circle this other)
+            (= other-class Rectangle)
+            (point-intersect-rectangle this other)
+            :else (throw (Exception. (str "Ops. Other class: " (class other))))))))
+
+(extend-protocol Positioned
+  Circle
+  (position [this]
+    (:center this))
+
+  Rectangle
+  (position [{:keys [xmin xmax ymax ymin]}]
+    (let [xmid (* 0.5 (+ xmin xmax))
+          ymid (* 0.5 (+ ymin ymax))]
+      [xmid ymid]))
+
+  Point
+  (position [{:keys [x y]}]
+    [x y]))
+
+(extend-protocol Movable
+  Circle
+  (update-pos [this new-pos]
+    (-> this
+        (assoc :center new-pos)))
+
+  Rectangle
+  (update-pos [{:keys [xmin xmax ymin ymax]} new-pos]
+    (let [xmid (* 0.5 (+ xmin xmax))
+          ymid (* 0.5 (+ ymin ymax))
+          [dx dy] (pts/diff-pts new-pos [xmid ymid])]
+      (->Rectangle (+ xmin dx)
+                   (+ ymin dy)
+                   (+ xmax dx)
+                   (+ ymax dy)))))
